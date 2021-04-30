1f133e0e8f445a44b14b613f9d74df6464416465
Himanshu Mishra <hm221311@gmail.com>
2021-04-30 20:28:22 +0530

    schema design for order.js

--- models/orders.js
+++ models/orders.js
@@ -3,7 +3,22 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const order_schema = new Schema({
-    
+    user_id: String,
+    user_name: String,
+    user_mobile: Number,
+    user_email: String,
+    user_addresses:[{
+        name: String,
+        mobile: Number,
+        address: [{
+            house_no: Number,
+            street: String,
+            pincode: Number,
+            city: String,
+            state: String
+        }]
+
+    }]
 });
 
 const orders = mongoose.model('orders', order_schema);
